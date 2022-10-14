c1e41d47ddbbab46cbd0d4ec031f2486294e7ef1
LeeKien90 <112063096+LeeKien90@users.noreply.github.com>
2022-10-14 10:46:21 +0700

    Add files via upload

--- MD2_Bai13_BT3/src/Main.java
+++ MD2_Bai13_BT3/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+
+        System.out.println("Nhap chuoi ");
+        String string = sc.nextLine();
+
+        //Tim chuoi lien tiep co do dai lon nhat
+        for (int i = 0; i < string.length(); i++) {
+            if (list.size()>1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
+
+                list.clear();//1 cau lenh
+            }
+            list.add(string.charAt(i));
+
+            if (list.size() > max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+        }
+        for(Character ch : max){
+            System.out.println(ch);
+        }
+        System.out.println();
     }
 }
